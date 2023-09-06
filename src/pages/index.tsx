309ac4b4675d3147d04fe4e8ebce09310c3573c0
AvishekThapa1997 <avishekthapa1997@gmail.com>
2023-09-06 20:33:06 +0530

    Setup shadcn-ui

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,3 @@
-// import { Inter } from 'next/font/google';
-
-// const inter = Inter({ subsets: ['latin'] });
-
 export default function Home() {
-  return <h1>Open Table</h1>;
+  return <h1>Photo album</h1>;
 }
